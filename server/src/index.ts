470f7c167d5d3e02f9d5d1956acf1abac38efe98
Ngo Minh Khai <ngominhkhai28082007@gmail.com>
2026-06-09 22:38:53 +0700

    feat(server): attach Socket.io to HTTP server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,23 @@
 import "dotenv/config";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import { createApp } from "./app";
+import { registerRoomHandlers } from "./sockets/roomHandlers";
 
 const port = Number(process.env.PORT ?? 4000);
 const app = createApp();
+const httpServer = createServer(app);
 
-app.listen(port, () => {
+const allowedOrigin = process.env.CLIENT_ORIGIN
+  ? process.env.CLIENT_ORIGIN
+  : /^http:\/\/localhost:\d+$/;
+
+export const io = new Server(httpServer, {
+  cors: { origin: allowedOrigin },
+});
+
+registerRoomHandlers(io);
+
+httpServer.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}`);
 });
---
+++ server/src/sockets/roomHandlers.ts
@@ -0,0 +1,5 @@
+import { Server } from "socket.io";
+
+export function registerRoomHandlers(_io: Server): void {
+  // implemented in Task 4
+}
